800c45f4508a8e13f082469408d5dc5d8c78c0ff
Gonzalo Potente <gonzalopotente2772@gmail.com>
2022-10-10 19:31:21 -0300

    Primer commit - Agregando archivos.

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname,'./public');
+const port = process.env.PORT || 3001;
+app.use(express.static(publicPath));
+app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/home.html')))
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, './views/register.html')))
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, './views/login.html')))
